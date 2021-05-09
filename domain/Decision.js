b94b0701e5f02bbe1cbd817a725f40c5b3520d51
vcalatayud <lacuentadevictor@gmail.com>
2021-05-09 11:18:55 +0200

    adding tests for decision make decision asking everyone

--- domain/Decision.js
+++ domain/Decision.js
@@ -1,9 +1,35 @@
-module.exports = function(){
+const { maxConcurrentTestRunners } = require("../stryker.conf")
+
+module.exports = function({population}){
     return Object.freeze({
-        doStuff
+        make
     })
 
-    async function doStuff(){
-        return 'stuff done'
+    async function make(){
+        var sample = await getSampleSize()
+    }
+
+    async function getSampleSize(){
+        
+    }
+
+    async function askEveryone(){
+
+        var size = await population.getSize()
+        var results = {}
+        for(var i=0; i<size; i++){
+            var vote = await population.askIntentionTo(i)
+            if(results[vote]) results[vote]++
+            else results[vote] = 1
+        }
+
+        var options = Object.getOwnPropertyNames(results)
+
+        if(results[options[0]] > results[options[1]]) conclusion = options[0]
+        else conclusion = options[1]
+
+        return {
+            conclusion
+        }
     }
 }
